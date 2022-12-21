a2d461a33a5986e43dd884f6b5d280a2e6621297
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-21 17:40:22 +0530

    Cleaned up older code

--- src/components/table/SimpleTable.js
+++ src/components/table/SimpleTable.js
@@ -41,52 +41,8 @@ export const BasicTable = ({data, columns}) => {
   );
 
   return (
-      <>
-      {/*<table {...getTableProps()}>*/}
-      {/*  <thead>*/}
-      {/*  {headerGroups.map(headerGroup => (*/}
-      {/*      <tr {...headerGroup.getHeaderGroupProps()}>*/}
-      {/*        {*/}
-      {/*          headerGroup.headers.map(column => (*/}
-      {/*              <th {...column.getHeaderProps()}>{column.render('Header')}</th>*/}
-      {/*          ))*/}
-      {/*        }*/}
-      {/*      </tr>*/}
-      {/*  ))}*/}
-      {/*  </thead>*/}
-      {/*  <tbody {...getTableBodyProps()}>*/}
-      {/*  {*/}
-      {/*    rows.map(row => {*/}
-      {/*      prepareRow(row);*/}
-      {/*      return (*/}
-      {/*          <tr {...row.getRowProps()}>*/}
-      {/*            {*/}
-      {/*              row.cells.map(cell => {*/}
-      {/*                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>*/}
-      {/*              })*/}
-      {/*            }*/}
-      {/*          </tr>*/}
-      {/*      );*/}
-      {/*    })*/}
-      {/*  }*/}
-      {/*  </tbody>*/}
-      {/*  <tfoot>*/}
-      {/*  {*/}
-      {/*    footerGroups.map(footerGroup => (*/}
-      {/*        <tr {...footerGroup.getFooterGroupProps()}>*/}
-      {/*          {*/}
-      {/*            footerGroup.headers.map(column => (*/}
-      {/*                <td {...column.getFooterProps()}>*/}
-      {/*                  {column.render('Footer')}*/}
-      {/*                </td>*/}
-      {/*            ))*/}
-      {/*          }*/}
-      {/*        </tr>*/}
-      {/*    ))*/}
-      {/*  }*/}
-      {/*  </tfoot>*/}
-      {/*</table>*/}
-      <table {...getTableProps()}>
+  <>
+  <table {...getTableProps()}>
     <thead>
     {headerGroups.map(headerGroup => (
         <tr {...headerGroup.getHeaderGroupProps()}>
@@ -130,6 +86,6 @@ export const BasicTable = ({data, columns}) => {
     }
     </tfoot>
   </table>
-      </>
+  </>
   );
 }
