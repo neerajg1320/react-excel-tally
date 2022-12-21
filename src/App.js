c7c46bbf36b92f5a55bb10d433a0a4ab8d30b3b3
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-21 19:27:25 +0530

    Fixed the rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-// import './App.css';
 import {BasicTable} from "./components/table/SimpleTable";
-import {useCallback, useMemo, useState} from "react";
-import MOCK_DATA from "./assets/MOCK_DATA.json";
-import { MOCK_COLUMNS } from './assets/MOCK_COLUMNS';
 import ReadExcel from "./components/excel/xlsx/ReadExcel";
 
 
@@ -34,29 +30,29 @@ const Layout = () => {
   });
 
   return (
-      <>
-        <h1>React Router</h1>
+    <>
+      <h1>React Router</h1>
 
-        <nav
-            style={{
-              borderBottom: 'solid 1px',
-              paddingBottom: '1rem',
-              paddingLeft: "1rem",
-              display: "flex", flexDirection:"row", gap:"10px"
-            }}
-        >
-          <NavLink to="/read" style={style}>
-            Read
-          </NavLink>
-          <NavLink to="/table" style={style}>
-            Table
-          </NavLink>
-        </nav>
+      <nav
+          style={{
+            borderBottom: 'solid 1px',
+            paddingBottom: '1rem',
+            paddingLeft: "1rem",
+            display: "flex", flexDirection:"row", gap:"10px"
+          }}
+      >
+        <NavLink to="/read" style={style}>
+          Read
+        </NavLink>
+        <NavLink to="/table" style={style}>
+          Table
+        </NavLink>
+      </nav>
 
-        <main style={{ padding: '1rem 0' }}>
-          <Outlet />
-        </main>
-      </>
+      <main style={{ padding: '1rem 0' }}>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
@@ -64,44 +60,33 @@ const Read = () => {
   console.log(`Rendering <Read>`);
   const navigate = useNavigate();
 
-  const onLoadComplete = useCallback(({data}) => {
-    console.log(`data=${JSON.stringify(data, null, 2)}`);
-
+  const onLoadComplete = ({data}) => {
     navigate('/table', { state: { id: 9, just:"like", data } });
-  });
+  };
 
   return (
-      <>
-        <ReadExcel onComplete={onLoadComplete}/>
-      </>
+    <>
+      <ReadExcel onComplete={onLoadComplete}/>
+    </>
   );
 };
 
 const Table = () => {
   console.log(`Rendering <Table>`);
   const {state} = useLocation();
-  // const [data, setData] = useState(MOCK_DATA);
-  // const [columns, setColumns] = useState(MOCK_COLUMNS)
 
   if (!state) {
     return <h1>Empty</h1>
-    // TBD: We can probably move the column detection outside
   }
   const {data} = state;
   const columns = getColumns(data);
   const rtColumns = columns.map(colToRTCol);
-  console.log(`rtColumns=${JSON.stringify(rtColumns, null, 2)}`)
-
-
-  // const rtColumns = columns.map(col => {
-  //   return {...col, id: col.key, Header: col.label, accessor: col.key}
-  // });
-
+  // console.log(`rtColumns=${JSON.stringify(rtColumns, null, 2)}`)
 
   return (
-      <>
-        <BasicTable data={data} columns={rtColumns}/>
-      </>
+    <>
+      <BasicTable data={data} columns={rtColumns}/>
+    </>
   );
 };
 
